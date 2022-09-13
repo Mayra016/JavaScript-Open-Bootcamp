2e3181ccf59ce1d00b5e1d295ce9dc3795497b28
Mayra016 <111470957+Mayra016@users.noreply.github.com>
2022-09-12 22:13:49 -0300

    Create Tema-2-Ejercicio1.js

---
+++ Tema-2-Ejercicio1.js
@@ -0,0 +1,10 @@
+const fechaNacimiento = new Date("october 13 1997")
+const libroFav {
+    titulo: "Harry Potter y la cámara de los secretos",
+    autor: "J.K.Rowlin",
+    fecha: 1998,
+    url:"https://www.amazon.com/-/es/J-K-Rowling/dp/8478884955"
+}
+var lista = ["Máyra", 24, true, fechaNacimiento, libroFav.titulo, libroFav.autor, libroFav.fecha, libroFav.url]
+
+console.log(lista);
