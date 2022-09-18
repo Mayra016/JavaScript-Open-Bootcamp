fc3986a6d5dcdd9b0ea6b70c3ae788beff89e01e
Mayra016 <111470957+Mayra016@users.noreply.github.com>
2022-09-17 21:10:49 -0300

    Update Tema-5-Ejercicio-1.js

--- Tema-5-Ejercicio-1.js
+++ Tema-5-Ejercicio-1.js
@@ -3,14 +3,4 @@ let alturaM = new Number(1.70);
 let pesoKg = 56;
 let altura_M_Redondeada = alturaM.toFixed();
 let pesoKgAbajo = Math.floor(pesoKg);
-
-// Verifica si el máximo valor más 1 es igual al máximo valor.
-let max_Value = Number.MAX_VALUE + 1;
-let max_Value_JS = Number.MAX_VALUE;
-let compara = null;
-
-if (max_Value == max_Value_JS) {
-    compara = true;
-} else {
-    compara = false;
-}
+let compara = Number.MAX_VALUE + 1 == Number.MAX_VALUE;
