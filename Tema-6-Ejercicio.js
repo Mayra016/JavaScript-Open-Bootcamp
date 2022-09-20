258e5425e482b882d58a6a4e79b1914b3c431803
Mayra016 <111470957+Mayra016@users.noreply.github.com>
2022-09-19 22:20:22 -0300

    Create Tema-6-Ejercicio.js

---
+++ Tema-6-Ejercicio.js
@@ -0,0 +1,27 @@
+let listaCompra = ["aguacate", "huevos", "sal", "shoyu", "maíz"];
+listaCompra.push("Aceite de Girasol");
+
+listaCompra.pop("Aceite de Girasol");
+const pelisFav = [
+    { titulo : "Harry Potter y la cámara de los secretos", director : "Chris Columbus", fecha : 2002 },
+    { titulo : "El teléfono negro", director : "Scott Derrickson", fecha : 2022 },
+    { titulo : "Presagio", director : "Alex Proyas", fecha : 2009 }
+    ]
+    
+const pelisDespues2010 = pelisFav.filter(pelis => { 
+    if (pelis.fecha > 2010) {
+        return true
+    }    
+})
+
+const listaDirectores = pelisFav.map(pelis => {
+    return pelis.director
+})
+
+const listaTitulos = pelisFav.map(pelis => {
+    return pelis.titulo
+})
+
+const tituloyDirectores = listaTitulos.concat(listaDirectores);
+
+const propagacion = listaDirectores.map((valor, indice) => '${valor} - ${listaTitulos[indice]');
